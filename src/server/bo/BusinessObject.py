98f047f9488e4915d224eae18ce131e4fd45811f
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2021-05-06 10:45:35 +0200

    Bo

---
+++ src/server/bo/BusinessObject.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class BusinessObject(ABC):
+
+    def __init__(self):
+        self._id = 0
+        self._creation_date = 0
+
+    def get_id(self):
+        return self._id
+
+    def set_id(self,value):
+        self._id = value
+
+    def get_creation_date(self):
+        return self._creation_date
+
+    def set_creation_date(self, new_date):
+        self._creation_date = new_date
