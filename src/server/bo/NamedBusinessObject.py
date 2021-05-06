4c40c9e3d6daaf521941b4222bc27e546aa6dfc1
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2021-05-06 12:28:02 +0200

    Named Business Obj Class & Anpassung in anderen Klassen

---
+++ src/server/bo/NamedBusinessObject.py
@@ -0,0 +1,14 @@
+from src.server.bo import BusinessObject as bo
+
+
+class NamedBusinessObject(bo.BusinessObject):
+
+    def __init__(self):
+        super().__init__()
+        self._name = ""
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, value):
+        self._name = value
