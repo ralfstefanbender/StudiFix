bdd393636a29a3000c4b3333f0dbadcf59a928d3
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2021-05-24 11:34:46 +0200

    Learningprofile wird gesplittet in User und group

---
+++ src/server/bo/LearningProfileUser.py
@@ -0,0 +1,82 @@
+from src.server.bo import LearningProfile as bo
+
+
+class LearningProfile(bo.LearningProfile):
+
+    def __init__(self):
+        super().__init__()
+        self._frequency = 0
+        self._study_state = 0
+        self._extroversion = 0
+        self._prev_knowledge = 0
+        self._learntyp = 0  # Lerntypen sind kategorisiert nach Nummern
+        self._interest = ""
+        self._semester = 0
+        self._degree_course = ""
+
+    def get_frequency(self):
+        return self._frequency
+
+    def set_frequency(self, value):
+        self._frequency = value
+
+    def get_study_state(self):
+        return self._study_state
+
+    def set_study_state(self, value):
+        self._study_state = value
+
+    def get_extroversion(self):
+        return self._extroversion
+
+    def set_extroversion(self, value):
+        self._extroversion = value
+
+    def get_prev_knowledge(self):
+        return self._prev_knowledge
+
+    def set_prev_knowledge(self, value):
+        self._prev_knowledge = value
+
+    def get_learntyp(self):
+        return self._learntyp
+
+    def set_learntyp(self, value):
+        self._learntyp = value
+
+    def get_interest(self):
+        return self._interest
+
+    def set_interest(self, value):
+        self._interest = value
+
+    def get_semester(self):
+        return self._semester
+
+    def set_semester(self, value):
+        self._semester = value
+
+    def get_degree_course(self):
+        return self._degree_course
+
+    def set_degree_course(self, value):
+        self._degree_course = value
+
+
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """Umwandeln eines Python dict() in einen User()."""
+        obj = LearningProfile()
+        obj.set_id(dictionary["id"])
+        obj.set_creation_date(dictionary["creation_date"])
+        obj.set_name(dictionary["name"])
+        obj.set_frequency(dictionary["frequency"])
+        obj.set_study_state(dictionary["study_state"])
+        obj.set_extroversion(dictionary["extroversion"])
+        obj.set_prev_knowledge(dictionary["prev_knowledge"])
+        obj.set_learntyp(dictionary["learntyp"])
+        obj.set_interest(dictionary["interest"])
+        obj.set_semester(dictionary["semester"])
+        obj.set_degree_course(dictionary["degree_course"])
+        return obj
