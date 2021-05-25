4078774b16d4b9f0f6d5e9018736b2e9b524e078
fatihuenal <fatih-uenal@gmx.de>
2021-05-25 13:26:44 +0200

    LearningProfileUserMapper angepasst

--- src/server/db/LearningProfileUserMapper.py
+++ src/server/db/LearningProfileUserMapper.py
@@ -14,38 +14,38 @@ class LearningProfileUserMapper(Mapper):
         if len(tuples) == 1:
             for (id, user_id, name, prev_knowledge, extroversion, study_state, frequency,
                  learntyp, semester, interest, degree_course, creation_date) in tuples:
-                learning_profile = LearningProfileUser()
-                learning_profile.set_id(id)
-                learning_profile.set_user_id(user_id)
-                learning_profile.set_name(name)
-                learning_profile.set_prev_knowledge(prev_knowledge)
-                learning_profile.set_extroversion(extroversion)
-                learning_profile.set_study_state(study_state)
-                learning_profile.set_frequency(frequency)
-                learning_profile.set_learntyp(learntyp)
-                learning_profile.set_semester(semester)
-                learning_profile.set_interest(interest)
-                learning_profile.set_degree_course(degree_course)
-                learning_profile.set_creation_date(creation_date)
-                result = learning_profile
+                learning_profile_user = LearningProfileUser()
+                learning_profile_user.set_id(id)
+                learning_profile_user.set_user_id(user_id)
+                learning_profile_user.set_name(name)
+                learning_profile_user.set_prev_knowledge(prev_knowledge)
+                learning_profile_user.set_extroversion(extroversion)
+                learning_profile_user.set_study_state(study_state)
+                learning_profile_user.set_frequency(frequency)
+                learning_profile_user.set_learntyp(learntyp)
+                learning_profile_user.set_semester(semester)
+                learning_profile_user.set_interest(interest)
+                learning_profile_user.set_degree_course(degree_course)
+                learning_profile_user.set_creation_date(creation_date)
+                result = learning_profile_user
 
         else:
             for (id, user_id, name, prev_knowledge, extroversion, study_state, frequency,
                  learntyp, semester, interest, degree_course, creation_date) in tuples:
-                learning_profile = LearningProfileUser()
-                learning_profile.set_id(id)
-                learning_profile.set_user_id(user_id)
-                learning_profile.set_name(name)
-                learning_profile.set_prev_knowledge(prev_knowledge)
-                learning_profile.set_extroversion(extroversion)
-                learning_profile.set_study_state(study_state)
-                learning_profile.set_frequency(frequency)
-                learning_profile.set_learntyp(learntyp)
-                learning_profile.set_semester(semester)
-                learning_profile.set_interest(interest)
-                learning_profile.set_degree_course(degree_course)
-                learning_profile.set_creation_date(creation_date)
-                result.append(learning_profile)
+                learning_profile_user = LearningProfileUser()
+                learning_profile_user.set_id(id)
+                learning_profile_user.set_user_id(user_id)
+                learning_profile_user.set_name(name)
+                learning_profile_user.set_prev_knowledge(prev_knowledge)
+                learning_profile_user.set_extroversion(extroversion)
+                learning_profile_user.set_study_state(study_state)
+                learning_profile_user.set_frequency(frequency)
+                learning_profile_user.set_learntyp(learntyp)
+                learning_profile_user.set_semester(semester)
+                learning_profile_user.set_interest(interest)
+                learning_profile_user.set_degree_course(degree_course)
+                learning_profile_user.set_creation_date(creation_date)
+                result.append(learning_profile_user)
 
         return result
 
@@ -71,7 +71,7 @@ class LearningProfileUserMapper(Mapper):
 
         cursor = self._cnx.cursor()
         command = "SELECT id, user_id, name, prev_knowledge, extroversion, study_state, frequency," \
-                  "learntyp, semester, interest, degree_course, creation_date FROM learning_profile " \
+                  "learntyp, semester, interest, degree_course, creation_date FROM learning_profile_user " \
                   "WHERE id LIKE '{}' ".format(id)
         cursor.execute(command)
         tuples = cursor.fetchall()
@@ -80,7 +80,31 @@ class LearningProfileUserMapper(Mapper):
             result = self.build_bo(tuples)
 
         except IndexError:
-            """Falls kein LearningProfile User mit der angegebenen id gefunden werden konnte,
+            """Falls kein LearningProfile User mit der angegebenen user_id gefunden werden konnte,
+                wird hier None als Rückgabewert deklariert"""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_user_id(self, user_id):
+
+        result = None
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, user_id, name, prev_knowledge, extroversion, study_state, frequency," \
+                  "learntyp, semester, interest, degree_course, creation_date FROM learning_profile_user " \
+                  "WHERE user_id LIKE '{}' ".format(user_id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            result = self.build_bo(tuples)
+
+        except IndexError:
+            """Falls kein LearningProfile User mit der angegebenen user_id gefunden werden konnte,
                 wird hier None als Rückgabewert deklariert"""
             result = None
 
@@ -95,7 +119,7 @@ class LearningProfileUserMapper(Mapper):
 
         cursor = self._cnx.cursor()
         command = "SELECT id, user_id, name, prev_knowledge, extroversion, study_state, frequency," \
-                  "learntyp, semester, interest, degree_course, creation_date FROM learning_profile " \
+                  "learntyp, semester, interest, degree_course, creation_date FROM learning_profile_user " \
                   "WHERE name LIKE '{}' ".format(name)
         cursor.execute(command)
         tuples = cursor.fetchall()
@@ -112,7 +136,7 @@ class LearningProfileUserMapper(Mapper):
 
         return result
 
-    def insert(self, learning_profile):
+    def insert(self, learning_profile_user):
 
         cursor = self._cnx.cursor()
         cursor.execute("SELECT MAX(id) as maxid from learning_profile_user")
@@ -120,59 +144,61 @@ class LearningProfileUserMapper(Mapper):
 
         for (maxid) in tuples:
             if maxid[0] is None:
-                learning_profile.set_id(1)
+                learning_profile_user.set_id(1)
             else:
-                learning_profile.set_id(maxid[0] + 1)
+                learning_profile_user.set_id(maxid[0] + 1)
 
-        command = "INSERT INTO learning_profile_user (id, user_id, name, prev_knowledge, extroversion, study_state, frequency," \
+        command = "INSERT INTO learning_profile_user (id, user_id, name, prev_knowledge," \
+                  "extroversion, study_state, frequency," \
                   "learntyp, semester, interest, degree_course, creation_date) VALUES " \
                   "('{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}','{}')" \
-            .format(learning_profile.get_id(),
-                    learning_profile.get_user_id(),
-                    learning_profile.get_name(),
-                    learning_profile.get_prev_knowledge(),
-                    learning_profile.get_extroversion(),
-                    learning_profile.get_study_state(),
-                    learning_profile.get_frequency(),
-                    learning_profile.get_learntyp(),
-                    learning_profile.get_semester(),
-                    learning_profile.get_interest(),
-                    learning_profile.get_degree_course(),
-                    learning_profile.get_creation_date())
+            .format(learning_profile_user.get_id(),
+                    learning_profile_user.get_user_id(),
+                    learning_profile_user.get_name(),
+                    learning_profile_user.get_prev_knowledge(),
+                    learning_profile_user.get_extroversion(),
+                    learning_profile_user.get_study_state(),
+                    learning_profile_user.get_frequency(),
+                    learning_profile_user.get_learntyp(),
+                    learning_profile_user.get_semester(),
+                    learning_profile_user.get_interest(),
+                    learning_profile_user.get_degree_course(),
+                    learning_profile_user.get_creation_date())
         cursor.execute(command)
 
         self._cnx.commit()
         cursor.close()
 
-        return learning_profile
+        return learning_profile_user
 
-    def update(self, learning_profile):
+    def update(self, learning_profile_user):
 
         cursor = self._cnx.cursor()
-        command = "UPDATE learning_profile_user SET name = ('{}'), user_id = ('{}'), prev_knowledge = ('{}'), extroversion = ('{}')," \
-                  " study_state = ('{}'), frequency = ('{}'), learntyp = ('{}'), semester = ('{}')," \
-                  " interest = ('{}'), degree_course = ('{}'), creation_date = ('{}') WHERE id = ('{}')" \
-            .format(learning_profile.get_id(),
-                    learning_profile.get_user_id(),
-                    learning_profile.get_name(),
-                    learning_profile.get_prev_knowledge(),
-                    learning_profile.get_extroversion(),
-                    learning_profile.get_study_state(),
-                    learning_profile.get_frequency(),
-                    learning_profile.get_learntyp(),
-                    learning_profile.get_semester(),
-                    learning_profile.get_interest(),
-                    learning_profile.get_degree_course(),
-                    learning_profile.get_creation_date())
+        command = "UPDATE learning_profile_user SET name = ('{}'), user_id = ('{}'), prev_knowledge = ('{}')," \
+                  "extroversion = ('{}'), study_state = ('{}'), frequency = ('{}'), learntyp = ('{}')," \
+                  "semester = ('{}'), interest = ('{}'), degree_course = ('{}'), creation_date = ('{}')" \
+                  "WHERE id = ('{}')" \
+            .format(learning_profile_user.get_id(),
+                    learning_profile_user.get_user_id(),
+                    learning_profile_user.get_name(),
+                    learning_profile_user.get_prev_knowledge(),
+                    learning_profile_user.get_extroversion(),
+                    learning_profile_user.get_study_state(),
+                    learning_profile_user.get_frequency(),
+                    learning_profile_user.get_learntyp(),
+                    learning_profile_user.get_semester(),
+                    learning_profile_user.get_interest(),
+                    learning_profile_user.get_degree_course(),
+                    learning_profile_user.get_creation_date())
         cursor.execute(command)
 
         self._cnx.commit()
         cursor.close()
 
-    def delete(self, learning_profile):
+    def delete(self, learning_profile_user):
 
         cursor = self._cnx.cursor()
-        command = "DELETE FROM learning_profile_user WHERE id = ('{}')".format(learning_profile.get_id())
+        command = "DELETE FROM learning_profile_user WHERE id = ('{}')".format(learning_profile_user.get_id())
         cursor.execute(command)
 
         self._cnx.commit()
