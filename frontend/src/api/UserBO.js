95cb47d3e8630f751f17be6251a9a0b9295656bb
fatihuenal <fatih-uenal@gmx.de>
2021-06-08 14:54:44 +0200

    UserBO erstellt + Rechtschreibfehler korrigiert

---
+++ frontend/src/api/UserBO.js
@@ -0,0 +1,68 @@
+import BusinessObject from './BusinessObject';
+
+/**
+ * Represents a user
+ */
+export default class UserBO extends User {
+    /**
+   * Constructs a new User
+   */
+    constructor(aGoogleId, aFirstName, aLastName, aEMail, aAdress) {
+        super();
+        this.google_id = aGoogleId;
+        this.firstname = aFirstName;
+        this.lastname = aLastName;
+        this.email = aEMail;
+        this.adress = aAdress;
+    }
+
+    /** Sets the google_id of the user */
+    setGoogleId(aGoogleId){
+        this.google_id = aGoogleId;
+    }
+
+    /** Gets the google_id from the user */
+    getGoogleId(){
+        return this.google_id;
+    }
+
+    /** Sets the firstname of the user */
+    setFirstName(aFirstName){
+        this.firstname = aFirstName;
+    }
+
+    /** Gets the firstname of the user */
+    getFirstName(){
+        return this.firstname;
+    }
+
+    /** Sets the lastname of the user */
+    setLastName(aLastName){
+        this.lastname = aLastName;
+    }
+
+    /** Gets the lastname of the user */
+    getLastName(){
+        return this.lastname;
+    }
+
+    /** Sets the email of the user */
+    setEMail(aEMail){
+        this.email = aEMail;
+    }
+
+    /** Gets the email of the user */
+    getEMail(){
+        return this.email;
+    }
+
+    /** Sets the adress of the user */
+    setAdress(aAdress){
+        this.adress = aAdress;
+    }
+
+    /** Gets the adress of the user */
+    getAdress(){
+        return this.adress;
+    }
+}
