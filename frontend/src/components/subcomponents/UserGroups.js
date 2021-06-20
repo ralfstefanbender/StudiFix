9f143f61238811a9c49b27c4c4a34458639b147e
fatihuenal <fatih-uenal@gmx.de>
2021-06-20 12:05:17 +0200

    "Lernpartner" changes

--- frontend/src/components/subcomponents/UserGroups.js
+++ frontend/src/components/subcomponents/UserGroups.js
@@ -1,7 +1,62 @@
-import React from 'react'
-import { makeStyles, Paper, Typography, Link } from '@material-ui/core';
+import React, { Component } from 'react'
+import { makeStyles, withStyles, Paper, Typography, Link, Grid } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import LoadingProgress from '../dialogs/LoadingProgress';
+import StudyFixAPI from '../../api/StudyFixAPI';
 
-const useStyles = makeStyles(theme => ({
+
+class UserGroups extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      buddys: [],
+      loadingInProgress: false,
+      error: null
+    };
+  }
+
+  /** Fetches ChatInvitationBOs for current user */
+  getChatInvitationAcceptedInvitesSource = (suser) => {
+    StudyFixAPI.getAPI().getChatInvitationAcceptedInvitesSource(suser)
+    .then(chatInvitationBOs => this.setState({
+      buddys: chatInvitationBOs,
+      filteredBuddys: [...chatInvitationBOs],
+      loadingInProgress: false,
+      error: null
+    })).catch(e => this.setState({
+      buddys: [],
+      loadingInProgress: false,
+      error: e
+    }));
+
+    this.setState({
+      loadingInProgress: true,
+      error: null
+    });
+  }
+
+/** Lifecycle method, which is called when the component gets inserted into the browsers DOM */
+componentDidMount() {
+  this.getChatInvitationAcceptedInvitesSource();
+}
+
+render(){
+  const { classes,} = this.props;
+  const { buddys, loadingInProgress, error} = this.state;
+
+  return(
+    <div className = {classes.root}>
+        <h1>Hier stehen ihre Lernpartner:</h1>
+    </div>
+  )
+}
+
+}
+
+
+
+const styles = theme => ({
   root: {
     width: '100%',
     marginTop: theme.spacing(2),
@@ -11,26 +66,7 @@ const useStyles = makeStyles(theme => ({
   content: {
     margin: theme.spacing(1),
   }
-}));
+});
 
-/**
- * Shows the about page with the impressum
- *
- * @author [Christoph Kunz](https://github.com/christophkunz)
- */
-function UserGroups() {
 
-  const classes = useStyles();
-
-  return (
-    <Paper elevation={0} className={classes.root}>
-      <div className={classes.content}>
-        <Typography variant='h6'>
-       Lernpartner
-        </Typography>
-      </div>
-    </Paper>
-  )
-}
-
-export default UserGroups;
+export default withStyles(styles)(UserGroups);
