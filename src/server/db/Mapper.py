37af4411fe6d8f22e115fa36015c1137803dd8ab
wasja262 <78151682+WilliKoljada@users.noreply.github.com>
2021-07-01 10:41:37 +0200

    Kommentare in der Mapper.py hinzugefügt.

--- src/server/db/Mapper.py
+++ src/server/db/Mapper.py
@@ -13,11 +13,21 @@ class Mapper (AbstractContextManager, ABC):
     def __enter__(self):
 
         if True:
+
+            """Wenn wir hier ankommen, dann handelt sich offenbar um die Ausführung des Codes in einer lokalen Umgebung,
+            also auf einem Local Development Server. Hierbei stellen wir eine einfache Verbindung zu einer lokal
+            installierten mySQL-Datenbank her."""
+
             self._cnx = connector.connect(user='root', password="root",
                                       host='127.0.0.1',
                                       database='studi_fix')
 
         else:
+
+            """Landen wir in diesem Zweig, so haben wir festgestellt, dass der Code in der Cloud abläuft.
+            Die App befindet sich somit im **Production Mode** und zwar im *Standard Environment*.
+            Hierbei handelt es sich also um die Verbindung zwischen Google App Engine und Cloud SQL."""
+
             self._cnx = connector.connect(user='root', password="itprojekt21",
                                     host='34.141.112.201',
                                     database='studi_fix_database')
